c4e2a36b666848bf61cee70d8c1ebfc21e0f27de
neo <quangman1404@gmail.com>
2020-07-02 10:00:48 +0700

    added semantic ui

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,4 +1,26 @@
+import React, { useState } from 'react'
+import { Menu } from 'semantic-ui-react'
+
 function Index() {
-    return <h1>This is next js bitches!</h1>;
+    const [activeMenu, setMenu] = useState('home')
+    return (
+        <Menu pointing secondary vertical>
+            <Menu.Item
+                name='home'
+                active={activeMenu === 'home'}
+                onClick={() => setMenu('home')}
+            />
+            <Menu.Item
+                name='messages'
+                active={activeMenu === 'messages'}
+                onClick={() => setMenu('messages')}
+            />
+            <Menu.Item
+                name='friends'
+                active={activeMenu === 'friends'}
+                onClick={() => setMenu('friends')}
+            />
+        </Menu>
+    )
 }
 export default Index;
