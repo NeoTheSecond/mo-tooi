beabdb8537385cd974a0165f04cfd5c0f4bcdc59
neo <quangman1404@gmail.com>
2020-07-04 09:26:30 +0700

    styling to home page

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,11 +1,11 @@
 import NavBar from "./Navbar"
-
-const layoutStyle = {
-    display: "flex",
-    flexDirection: "row",
-    height: "100%",
-    width: "100%"
-};
+import { Grid } from "semantic-ui-react"
+// const layoutStyle = {
+//     display: "flex",
+//     flexDirection: "row",
+//     height: "100%",
+//     width: "100%"
+// };
 
 const contentStyle = {
     flex: 1,
@@ -14,12 +14,23 @@ const contentStyle = {
 };
 
 const Layout = props => (
-    <div className="Layout" style={layoutStyle}>
-        <NavBar />
-        <div className="Content" style={contentStyle}>
-            {props.children}
-        </div>
-    </div>
+    <Grid columns={3} >
+        <Grid.Row style={{ paddingBottom: "0px" }}>
+            <Grid.Column width={4} style={{ height: "100vh", backgroundColor: "#f5e8d7" }}>
+                <NavBar />
+            </Grid.Column>
+            <Grid.Column width={12}>{props.children}</Grid.Column>
+        </Grid.Row>
+    </Grid>
 )
+
+// const Layout = props => (
+//     <div className="Layout" style={layoutStyle}>
+//         <NavBar />
+//         <div className="Content" style={contentStyle}>
+//             {props.children}
+//         </div>
+//     </div>
+// )
 
 export default Layout;
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,31 +1,45 @@
 import React, { useState } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Image, Container } from 'semantic-ui-react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Navbar() {
-    const [activeMenu, setMenu] = useState(null)
+    const router = useRouter()
+    const [activeMenu, setMenu] = useState(router.pathname)
+    console.log(router.pathname);
+    console.log(activeMenu);
+
+
     return (
-        <Menu pointing secondary vertical>
+        <Menu fluid secondary text vertical  >
+            <Menu.Item>
+                <Link href="/">
+                    <img src='/images/mo-tooi-logo.png' />
+                </Link>
+            </Menu.Item>
             <Link href="/poems">
                 <Menu.Item
                     name='Poems'
-                    active={activeMenu === 'Poems'}
-                    onClick={() => setMenu('Poems')}
+                    active={"/poems" === router.pathname}
+                    onClick={() => setMenu('/poems')}
+                    style={{ color: "/poems" === router.pathname ? "#ca5e42" : "#ebae8d" }}
                 />
             </Link>
             <Link href="/writings">
                 <Menu.Item
                     name='Writings'
-                    active={activeMenu === 'Writings'}
-                    onClick={() => setMenu('Writings')}
+                    active={"/writings" === router.pathname}
+                    onClick={() => setMenu('/writings')}
+                    style={{ color: "/writings" === router.pathname ? "#ca5e42" : "#ebae8d" }}
                 />
             </Link>
 
             <Link href="/about">
                 <Menu.Item
                     name='About'
-                    active={activeMenu === 'About'}
-                    onClick={() => setMenu('About')}
+                    active={"/about" === router.pathname}
+                    onClick={() => setMenu('/about')}
+                    style={{ color: "/about" === router.pathname ? "#ca5e42" : "#ebae8d" }}
                 />
             </Link>
 
