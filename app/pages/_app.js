4df6861f097bdc32271f74ad34d1b332d7d16fca
neo <quangman1404@gmail.com>
2020-07-09 19:24:06 +0700

    overflow hidden

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,8 +1,9 @@
-import 'semantic-ui-less/semantic.less'
 import Layout from "../components/Layout"
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { AnimatePresence } from 'framer-motion';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '../components/theme';
+import './styles.less'
 
 
 function MyApp({ Component, pageProps, router }) {
@@ -16,6 +17,7 @@ function MyApp({ Component, pageProps, router }) {
     return (
         <React.Fragment>
             <ThemeProvider theme={theme}>
+                <CssBaseline />
                 <Layout>
                     <AnimatePresence exitBeforeEnter>
                         <Component {...pageProps} key={router.route} />
